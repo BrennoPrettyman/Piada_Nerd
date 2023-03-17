50917f12c1d4e43ca7af8eac47ef13a93b9c3999
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 20:12:56 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Piada_Nerd;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("PIADA NERD");
+        Thread.Sleep(2000);
+        Console.WriteLine();
+        Console.WriteLine("Por que o Mario perdeu para o Sonic?");
+        Thread.Sleep(3000);
+        Console.WriteLine("Porque a justiça é SEGA!");
+        Thread.Sleep(3000);
+    }
+}
